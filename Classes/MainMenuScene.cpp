7b6d26f5d0ac02524744b7a6b69f621ea2f497b4
w016869e <w016869e@student.staffs.ac.uk>
2015-12-09 15:44:26 +0000

    Main menu button implementation
    Adding transitions when a button is pressed and properly seting up
    buttons.

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -1,24 +1,27 @@
 #include "MainMenuScene.h"
+#include "GameScene.h"
 #include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
 
 USING_NS_CC;
 
 using namespace cocostudio::timeline;
+using namespace cocos2d;
+using namespace cocos2d::ui;
 
 Scene* MainMenuScene::createScene()
 {
 	// 'scene' is an autorelease object
-	auto scene = Scene::create();
+	auto myScene = Scene::create();
 
 	// 'layer' is an autorelease object
 	auto layer = MainMenuScene::create();
 
 	// add layer as a child to scene
-	scene->addChild(layer);
+	myScene->addChild(layer);
 
 	// return the scene
-	return scene;
+	return myScene;
 }
 
 // on "init" you need to initialize your instance
@@ -35,5 +38,25 @@ bool MainMenuScene::init()
 
 	addChild(rootNode);
 
+	//Button children
+	startButton = static_cast<ui::Button*>(rootNode->getChildByName("btnStart"));
+	quitButton = static_cast<ui::Button*>(rootNode->getChildByName("btnQuit"));
+
+	//Button listeners
+	startButton->addTouchEventListener(CC_CALLBACK_2(MainMenuScene::StartButtonPressed, this)); // need to change to health/hp
+	quitButton->addTouchEventListener(CC_CALLBACK_2(MainMenuScene::QuitButtonPressed, this));
+
 	return true;
+}
+
+void MainMenuScene::StartButtonPressed(Ref *pSender, cocos2d::ui::Widget::TouchEventType type)
+{
+	CCLOG("In touch! %d", type);
+
+	if (type == cocos2d::ui::Widget::TouchEventType::ENDED)
+	{
+		Scene *scene = MainMenuScene::createScene();
+		Director::getInstance()->replaceScene(scene);
+	}
+
 }
